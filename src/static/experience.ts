974b429a90ee7d0c9182358375c59e1e3a09cb40
Mazouzi Aymene <39919087+El-Mazouzi@users.noreply.github.com>
2025-01-27 10:41:14 +0100

    updated experience and cv

--- src/static/experience.ts
+++ src/static/experience.ts
@@ -5,10 +5,10 @@ const EXPERIENCE = [
 		company: 'YASSIR (Y Combinator W20)',
 		jobTitle: 'Mobile developer',
 		description: [
-			'Developed and maintained Yassir Food Delivery app, focusing on user experience and functionality enhancements.',
-			'Conducted performance analysis and implemented optimizations that increased app responsiveness, resulting in a smoother user experience.',
-			'Contributed to analysis and schema optimization sessions, improving database efficiency by 50%.',
-			'Collaborated with a team of 12 developers to integrate new features, ensuring cross-platform functionality.',
+			'Enhanced the Yassir Food Delivery app, improving user experience and integrating new features.',
+			'Optimized performance metrics, increasing app responsiveness and reducing load times.',
+			'Contributed to database schema optimization, improving efficiency by 50%.',
+			'Collaborated in a cross-functional team of 12 developers, ensuring seamless platform-wide functionality.',
 		],
 	},
 	{
@@ -17,11 +17,10 @@ const EXPERIENCE = [
 		company: 'M-FORMATIK',
 		jobTitle: 'Mobile Developer',
 		description: [
-			'Spearheaded the development of 15 mobile applications, including E-commerce, Delivery, Laboratory Management, and Stock Management, contributing to a 50% growth in business.',
-			'Actively participated in UI/UX discussions, significantly enhancing the visual appeal and user experience of applications.',
-			'Managed the complete product life cycle for 15 projects, from initial design to deployment.',
-			'Held the responsibility for publishing mobile apps, ensuring seamless availability to users.',
-			'Optimized application performance, resulting in a 30% increase in responsiveness and efficiency.',
+			'Led the development of 15+ mobile applications, including E-commerce, Delivery, and Stock Management systems, driving 50% business growth.',
+			'Delivered end-to-end product lifecycle management, ensuring timely deployment and robust performance.',
+			'Implemented advanced security measures, safeguarding apps against reverse engineering threats.',
+			'Mentored junior developers and conducted code reviews to maintain code quality standards.',
 		],
 	},
 	{
@@ -30,10 +29,9 @@ const EXPERIENCE = [
 		company: 'Freelancing',
 		jobTitle: 'Mobile Developer',
 		description: [
-			'Built a user-friendly platform linking farmers with buyers for agricultural products, achieving 1500 active monthly users.',
-			'Developed Honest-Inn, an app that connects users with job opportunities by completing missions in the hotels, boosting business engagement.',
-			'Created a Dictionary app for a private school, enhancing language learning.',
-			'Demonstrated community involvement by building a Flutter Package that has over 90% popularity.',
+			'Designed Mantoudj Bladi, a platform connecting farmers to buyers, supporting 1500 active users monthly.',
+			'Created Honest-Inn, linking users to job opportunities in the hospitality sector, enhancing engagement.',
+			'Developed a high-impact Flutter Package, achieving 90%+ popularity.',
 		],
 	},
 	{
